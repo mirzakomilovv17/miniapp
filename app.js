c1f82ee1b3922282dba975363d3090b7b1fa1835
mirzakomilovv17 <muhammadmirzakomilov7@gmail.com>
2025-12-11 03:33:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+async function sendData() {
+    const name = document.getElementById("name").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const status = document.getElementById("status");
+
+    if (!name || !phone) {
+        status.textContent = "Ma'lumot to‘liq emas!";
+        return;
+    }
+
+    const backendURL = "https://BACKEND-URL-RENDER-DA.onrender.com/save";
+
+    const body = {
+        user_id: tg.initDataUnsafe?.user?.id,
+        name,
+        phone
+    };
+
+    const response = await fetch(backendURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    });
+
+    if (response.ok) {
+        status.textContent = "Muvaffaqiyatli yuborildi!";
+        tg.close();
+    } else {
+        status.textContent = "Xatolik!";
+    }
+}
